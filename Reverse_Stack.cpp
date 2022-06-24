825ebdc05787939cbad9d0a6d2529443c1278d1a
Aryan Dev Shourie <78311368+Aryan2727-debug@users.noreply.github.com>
2022-06-24 10:48:30 +0530

    Update Reverse_Stack.cpp

--- Reverse_Stack.cpp
+++ Reverse_Stack.cpp
@@ -1,23 +1,82 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include <stack>
+#include <algorithm>
 using namespace std;
-
+int n=4;
+class Stack{
+    int *arr;
+    int top;
+    public:
+    Stack(){
+        arr=new int[n];
+        top=-1;
+    }
+    void pop(){
+        if(top==-1){
+            cout<<"The Stack is empty"<<endl;
+            return;
+        }
+        top--;
+    }
+    void push(int x){
+        if(top==n-1){
+            cout<<"The Stack is full"<<endl;
+            return;
+        }
+        top++;
+        arr[top]=x;
+    }
+    int print(){
+        if(top==-1){
+            cout<<"The Stack is empty"<<endl;
+            return -1;
+        }
+        return arr[top];
+    }
+    void reverseStack(){
+        if(top==-1){
+            cout<<"The Stack is empty"<<endl;
+            return;
+        }
+        reverse(arr,arr+n);
+    }
+};
 int main()
 {
-    stack<int> s;
+    Stack s;
     s.push(10);
     s.push(20);
     s.push(30);
     s.push(40);
     cout<<"The Stack is"<<endl;
-    cout<<"10"<<" "<<"20"<<" "<<"30"<<" "<<"40"<<endl;
+    cout<<s.print()<<" ";
+    s.pop();
+    cout<<s.print()<<" ";
+    s.pop();
+    cout<<s.print()<<" ";
+    s.pop();
+    cout<<s.print()<<" ";
+    s.pop();
+    cout<<endl;
+    s.pop();
+    
+    cout<<endl;
+    
     cout<<"The reversed Stack is"<<endl;
-    while(!s.empty()){
-        cout<<s.top()<<" ";
-        s.pop();
-    }
-    
-    
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.reverseStack();
+    cout<<s.print()<<" ";
+    s.pop();
+    cout<<s.print()<<" ";
+    s.pop();
+    cout<<s.print()<<" ";
+    s.pop();
+    cout<<s.print()<<" ";
+    s.pop();
+    cout<<endl;
+    s.pop();
     return 0;
 }
